66e7cbee39c288ed7ff690379325559d5dd77812
KingsleyIbe <ibekingsley66@gmail.com>
2021-07-22 20:21:36 +0100

    initial commit

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ const About = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p className="my-1">This is a full stack react app for saving contacts</p>  
+            <p className="bg-dark p">
+                <strong>Version: </strong>1.0.0
+                
+            </p>
+            <div>
+                <p>Developed By: <a href="https://kingsleyibe.net" target="_blank">kingsleyibe.net</a></p>
+            </div>
+        </div>
+    )
+}
+export default About
